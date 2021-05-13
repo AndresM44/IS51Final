cbdfa85ac6a23c2024619d4d540e3e1ce829071b
Andres Martinez <andres_m559@Andress-MBP.attlocal.net>
2021-05-13 16:32:39 -0700

    finished psuedo-code

--- final_exam.py
+++ final_exam.py
@@ -9,3 +9,34 @@ First function will output number of grades.
 Second function will loop through the scores and output the average.
 Third function will also loop and output the percantage that are above the average.
 """
+"""
+# function 1 
+def main():
+    file = open("Final.txt", 'r')
+    data = file.read
+    numbers = data.split()
+    print("Number of grades: ", len(numbers))
+# function 2 
+    infile = open("Final.txt", 'r')
+    total = 0
+    number_of_grades = 0
+    line = float(infile.readline())
+    while line != "":
+        number_of_grades += 1
+        total += float(line)
+        line = infile.readline()
+        average = total / number_of_grades
+    print("Average grade: ", average)
+# function 3
+def calculate_percent_above_average(numbers, average):
+    above_average = 0
+    length = len(numbers)
+    for index in range (length):
+        if numbers[index] > average:
+            above_average = above_average +1
+    return above_average / length
+
+calculate_percent_above_average()
+main()
+
+"""
