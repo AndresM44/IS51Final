90999fd3479fdd5efc12bbece81b262f8c426fd6
Andres Martinez <andres_m559@Andress-MBP.attlocal.net>
2021-05-13 16:47:09 -0700

    finished exam-end

--- final_exam.py
+++ final_exam.py
@@ -40,3 +40,31 @@ calculate_percent_above_average()
 main()
 
 """
+
+def main():
+    file = open("Final.txt", "r")
+    data = file.read()
+    numbers = data.split()
+    print("Number of grades: ", len(numbers))
+    infile = open("Final.txt", "r")
+    total = 0
+    number_of_lines = 0
+    line = float(infile.readline())
+    while line != "":
+        number_of_lines += 1
+        total += float(line)
+        line = infile.readline()
+        average = total / number_of_lines
+    print("Average grade: ", average)
+
+# def calculate_percent_above_average(numbers, average):
+#     above_average = 0
+#     length = len(numbers)
+#     for index in range (length):
+#         if numbers [index] > average:
+#             above_average = above_average +1
+#     return above_average / length
+#     print("Percentage of grades above average: ", (return) + "%")
+
+# calculate_percent_above_average()
+main()
