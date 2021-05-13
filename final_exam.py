394a2802fb5b3b10ade84cefcd9bb865d1d9d413
Andres Martinez <andres_m559@Andress-MBP.attlocal.net>
2021-05-13 15:47:46 -0700

    finished structured-english

---
+++ final_exam.py
@@ -0,0 +1,11 @@
+"""
+The text file Final.txt contains the student's grades on a final exam.
+This program is going to analyse the grades and return three functions.
+First, the program will output the total number of grades found on Final.txt.
+Then, it will calculate the average grade of the numbers by dividing the sum of the numbers by the total numbers.
+Afterwards, the program will output the percentage of grades that are above average.
+
+First function will output number of grades.
+Second function will loop through the scores and output the average.
+Third function will also loop and output the percantage that are above the average.
+"""
